1ae52cab71b133d5f7610e4c4ab8945a78da026d
ryanj <ryanj@LAPTOP-0LU7MCLC>
2020-12-03 21:17:18 -0800

    idk

--- HW5/src/HW5/Palindrome.java
+++ HW5/src/HW5/Palindrome.java
@@ -1,10 +1,22 @@
 package HW5;
 
 import java.util.ArrayDeque;
+import java.util.*;
 import java.util.Deque;
+import java.util.Stack;
 
 public class Palindrome
 {
+	private String inputString;
+	
+	private static Stack<Character> charStack;
+	
+	public Palindrome(String str)
+	{
+		inputString = str;
+		fillStack(str);
+	}
+	
 	private static Deque<Character> fillStack(String inputString) 
 	{
 		 Deque<Character> charStack = new ArrayDeque<>();
@@ -17,7 +29,6 @@ public class Palindrome
 	
 	private static String buildReverse(String inputString) 
 	{
-		 Deque<Character> charStack = fillStack(inputString);
 		 StringBuilder result = new StringBuilder();
 		 
 		 while (!charStack.isEmpty()) 
